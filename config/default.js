84a2208112534e2ded3c358a82761ee6b0be339f
anton-fursov <32865517+anton-fursov@users.noreply.github.com>
2018-01-06 13:43:04 +0200

    Update default.js

--- config/default.js
+++ config/default.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   secret: 'mysecret',
   mongoose: {
-    uri: 'mongodb://fursovanton27:Der654ab@cluster0-shard-00-00-msllr.mongodb.net:27017,cluster0-shard-00-01-msllr.mongodb.net:27017,cluster0-shard-00-02-msllr.mongodb.net:27017/my_db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+    uri: 'mongodb://LOGIN:PASS@cluster0-shard-00-00-msllr.mongodb.net:27017,cluster0-shard-00-01-msllr.mongodb.net:27017,cluster0-shard-00-02-msllr.mongodb.net:27017/my_db?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
   },
   crypto: {
     hash: {
